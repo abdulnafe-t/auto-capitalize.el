662ee9ca483a5534a9da39636786b25e351e9aed
abdulnafe-t <abdulnafe.toulaimat@gmail.com>
2026-07-10 12:40:50 +0200

    [docs] Update license information

--- auto-capitalize.el
+++ auto-capitalize.el
@@ -1,4 +1,4 @@
-;;; auto-capitalize.el --- Automatically capitalize (or upcase) words -*- lexical-binding: t; -*-
+;;; auto-capitalize.el --- Automatically capitalize words -*- lexical-binding: t; -*-
 
 ;; Copyright   1998,2001,2002,2005 Kevin Rodgers
 ;; Copyright   2026 Abdulnafé Toulaïmat
@@ -17,20 +17,18 @@
 ;; Keywords: text, wp, convenience
 ;; URL: https://github.com/abdulnafe-t/auto-capitalize-el
 
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License as
-;; published by the Free Software Foundation; either version 2 of
-;; the License, or (at your option) any later version.
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 
-;; This program is distributed in the hope that it will be
-;; useful, but WITHOUT ANY WARRANTY; without even the implied
-;; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-;; PURPOSE.  See the GNU General Public License for more details.
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
 
-;; You should have received a copy of the GNU General Public
-;; License along with this program; if not, write to the Free
-;; Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-;; MA 02111-1307 USA
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
